79408356d03495e80e79e35a7ebd825fe9ac50da
mbaybay-fs <mbaybay@fullscale.io>
2022-11-08 00:49:09 +0800

    Setup Database

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 
-$con = new mysqli('localhost', 'root', 'password', 'heroku-assessment');
+$con = new mysqli('us-cdbr-east-06.cleardb.net', 'bbb79c10ccb2a5', 'b7f9d71e', 'heroku_77cd6e2d0c8dc5f');
 
 if (!$con) die(mysqli_error($con));
